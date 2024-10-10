b7d8738aa1a7a4202b329e16302aa35e9a147658
Sanjay Babu Dasari <dasari002@outlook.com>
2024-10-11 00:11:09 +0530

    Add an Accordion v1 to code base

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
+
 export default function App(){
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
@@ -42,7 +60,7 @@ export default function App(){
 
     <TravelLanding />
     <FlashCards />
-    <Accordion />
+    <Accordion data={faqs}/>
     </>
 }
 
--- src/components/AccordionComponent/Accordion.js
+++ src/components/AccordionComponent/Accordion.js
@@ -1,27 +1,23 @@
-const faqs = [
-    {
-      title: "Where are these chairs assembled?",
-      text:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
-    },
-    {
-      title: "How long do I have to return my chair?",
-      text:
-        "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
-    },
-    {
-      title: "Do you ship to countries outside the EU?",
-      text:
-        "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-    }
-  ];
+import './Accordion.css';
+import { useState } from 'react';
 
-  export default function Accordion() {
+  export default function Accordion({data}) {
     return <div>
-        <h1>Accordion TODO</h1>
-
-        {/* {faqs.map((faq) => (    
-            <AccordionItem key={faq.title} {...faq} />
-        ))} */}
+        <h1>Accordion FAQs</h1>
+        <div className="accordion">
+         { data.map((d, i) => <AccordionItem key={i} num ={i} title={d.title} text={d.text} />)}
+        </div>
     </div>;
+  }
+
+  function AccordionItem({ num,title, text }) {
+    const [isOpen, setIsOpen] = useState(false);
+    return(
+    <div className={isOpen ? "item open" : "item"} onClick={() => setIsOpen(!isOpen)}>
+      <p className="number"> {num < 9 ? `0${num+1}`: num+1} </p>
+       <p className="text">{title}</p>
+       <p className="icon">{isOpen ? '-' : '+'}</p>
+       {isOpen && <div className="content-box">{text}</div>}
+    </div>
+    );
   }
