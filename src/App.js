3ce166fe166cc2228029f9abe564f0865a18a34a
Sanjay Babu Dasari <dasari002@outlook.com>
2024-10-12 00:37:27 +0530

    Reusable Button and Children props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import { useState } from "react";
+
 import Dates from "./components/DateComponent/Dates";
 import TravelLanding from "./components/TravelLandComponents/TravelLanding";
 import FlashCards from "./components/FlashCardComponents/FlashCards";
 import Accordion from "./components/AccordionComponent/Accordion";
+import Steps from "./components/StepsComponent/Steps";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
 
 const faqs = [
   {
@@ -29,39 +25,17 @@ const faqs = [
 ];
 
 export default function App(){
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
-   
 
-  function handlePrevious(){
-      if(step > 1) setStep((s) => s - 1);
-  }
-  function handleNext(){
-    if(step < 3) {
-    setStep((s) => s+1);
-  }  
-}
-  return <>
-    <button className="close" onClick={() => setIsOpen((isOp)=>!isOp)}>&times;</button>
-  { isOpen && <div className="steps">
-    <div className="numbers">
-      <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-      <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-      <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
-    </div>
-    <p className="message"> Step {step}: {messages[step - 1]}</p>
-    <div className="buttons">
-      <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handlePrevious}>Previous</button>
-      <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handleNext}>Next</button>
-    </div>
-   
-    </div>}
+   return(
+    <>
+    <Steps/>
     <Dates  />
-
     <TravelLanding />
     <FlashCards />
     <Accordion data={faqs}/>
     </>
+   );    
+    
 }
 
 
